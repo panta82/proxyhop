6fe6d0daf14356329d05a53f72b1ecf8d48ff5f6
Ivan Pantic <ivanpantic82@gmail.com>
2018-05-25 00:43:29 +0200

    Some improvements in installer

--- misc/install.sh
+++ misc/install.sh
@@ -37,8 +37,10 @@ fi
 
 url="https://github.com/panta82/proxyhop/releases/download/${VERSION}/proxyhop_${VERSION}_${os}_${arch}"
 echo "Downloading proxyhop $VERSION from $url"
-curl -sf -L $url --output /usr/local/bin/proxyhop || fatal "Failed to download proxyhop to /usr/local/bin/proxyhop"
+curl -sf -L $url --output /tmp/proxyhop || fatal "Failed to download proxyhop to /usr/local/bin/proxyhop"
 
+cp -f /usr/local/bin/proxyhop /tmp/proxyhop.prev
+mv /tmp/proxyhop /usr/local/bin/proxyhop
 chmod +x /usr/local/bin/proxyhop
 
 echo "Proxyhop $VERSION has been installed to /usr/local/bin/proxyhop"
